2f4fef2502565448375fcba86d002057b1ee30a4
Prasanna R <prasannnarajendran@gmail.com>
2024-05-28 11:08:41 +0530

    FC

---
+++ components/Counter/Counter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props { }
+
+const Counter = () => {
+  return <div>
+    <div className="mother">
+      Redux Counter
+    </div>
+  </div>
+}
+
+export default Counter;
